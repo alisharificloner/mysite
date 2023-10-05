b2a604a08388bcbbea208fc3c7aae5bc20306bba
alisharificloner <146194737+alisharificloner@users.noreply.github.com>
2023-10-05 16:13:08 +0330

    Add files via upload

---
+++ work.js
@@ -0,0 +1,25 @@
+$('.guitar').mouseover(function(){
+    $('.guitar').attr("class","guitar1");
+    $('.pInGuitar').css('display','block');
+  });
+  $('.guitar').mouseleave(function(){
+    $('.guitar1').attr("class","guitar");
+    $('.pInGuitar').css('display','none');
+  });
+  $('.programming').mouseover(function(){
+    $('.programming').attr("class","programming1");
+    $('.pInProgramming').css('display','block');
+  });
+  $('.programming').mouseleave(function(){
+    $('.programming1').attr('class','programming');
+    $('.pInProgramming').css('display','none');
+  });
+  $('.omran').mouseover(function(){
+    $('.omran').attr('class','omran1');
+    $('.pInOmran').css('display','block');
+  });
+  $('.omran').mouseleave(function(){
+    $('.omran1').attr('class','omran');
+    $('.pInOmran').css('display','none');
+  });
+ 
